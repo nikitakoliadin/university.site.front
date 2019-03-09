dcfdcf7928cc471c9260a074d8c6a50e5920e281
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-09 15:07:25 +0200

    Updated configureStore.js: - Added local storage;

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,11 +2,17 @@ import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import throttle from 'lodash.throttle';
 
+import {loadState, saveState} from './localStorage';
 import reducers from '../reducers/reducers';
 
 const configureStore = () => {
-    const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk, logger)));
+    const persistedState = loadState();
+    const store = createStore(reducers, persistedState, composeWithDevTools(applyMiddleware(thunk, logger)));
+    store.subscribe(throttle(() => {
+        saveState({});
+    }, 1000));
     if (process.env.NODE_ENV !== "production") {
         if (module.hot) {
             module.hot.accept("../reducers/reducers", () => {
