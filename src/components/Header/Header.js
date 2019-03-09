fe07efa2b2510335f13558faa6999f5a3eef7607
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-09 13:35:58 +0200

    Created Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="Header">
+        <img className="Header-image" src="./image/header.gif" align="left" alt="gif"/>
+        <span className="Header-text">
+			University App
+		</span>
+        <img className="Header-image" src="./image/header.gif" align="right" alt="gif"/>
+    </div>
+);
+
+export default Header;
