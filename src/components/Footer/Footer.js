3998e25341b9d4d9ace347b3e4ea426acb6b839c
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-10 11:21:34 +0200

    Created Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+    <div className="Footer">
+        <span className="Footer-text">University App</span>
+        <span className="Footer-text">© qThegamEp, 2019</span>
+    </div>
+);
+
+export default Footer;
