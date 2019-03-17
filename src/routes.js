d73905c544179912cb244156207e94ce02c87fa6
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-17 14:26:32 +0200

    Updated routes.js: - Added contacts route;

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import {Route, IndexRedirect} from 'react-router';
 import AppContainer from './components/App/AppContainer';
 import Main from './components/Main/Main';
 import About from './components/About/About';
+import Contacts from './components/Contacts/Contacts';
 
 export default (
     <Route path="/" component={AppContainer}>
         <IndexRedirect to="main"/>
         <Route path="/main" component={Main}/>
         <Route path="/about" component={About}/>
+        <Route path="/contacts" component={Contacts}/>
         <Route path="*" component={Main}/>
     </Route>
 );
