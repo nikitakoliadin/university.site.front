89b1ef70fadf3fd43b9aeadbcbf48dbb8b00b217
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-17 10:54:17 +0200

    Updated reducers.js: - Added menuReducer;

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,8 +1,11 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
+import menuReducer from './menuReducer';
+
 const reducers = combineReducers({
-    routing: routerReducer
+    routing: routerReducer,
+    menu: menuReducer
 });
 
 export default reducers;
