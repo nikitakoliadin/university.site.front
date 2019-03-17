89a46390d6572e7eb49c2d38f52d80264ca8ccfc
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-17 13:11:23 +0200

    Updated Menu.js: - Added HamburgerMenu label; - Added CheeseburgerMenu slides; - Added styles;

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,17 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Link} from 'react-router';
+import HamburgerMenu from 'react-hamburger-menu';
+import CheeseburgerMenu from 'cheeseburger-menu';
 
-const Menu = () => (
-    <div>
-        <Link to="/about">
-            <b>&nbsp;</b>
-            About
-        </Link>
-        <Link to="/main">
-            <b>&nbsp;</b>
-            Main
-        </Link>
-    </div>
-);
+const Menu = props => {
+    const {menu, openMenu, closeMenu} = props;
+    return (
+        <div className="Menu">
+            <HamburgerMenu
+                isOpen={menu.showMenu}
+                menuClicked={() => openMenu()}
+                width={32}
+                height={24}
+                strokeWidth={3}
+                rotate={0}
+                color="black"
+                borderRadius={0}
+                animationDuration={0.5}
+            />
+            <CheeseburgerMenu
+                isOpen={menu.showMenu}
+                closeCallback={() => closeMenu()}>
+                <div className="List">
+                    <div className="Item">
+                        <Link to="/main"
+                              onClick={() => closeMenu()}>
+                            Main
+                        </Link>
+                    </div>
+                    <div className="Item">
+                        <Link to="/about"
+                              onClick={() => closeMenu()}>
+                            About
+                        </Link>
+                    </div>
+                    <div className="Logo">
+                        <img className="Image" src="./image/header.gif" alt="gif"/>
+                    </div>
+                </div>
+            </CheeseburgerMenu>
+        </div>
+    );
+};
+
+Menu.propTypes = {
+    menu: PropTypes.object.isRequired,
+    openMenu: PropTypes.func.isRequired,
+    closeMenu: PropTypes.func.isRequired
+};
 
 export default Menu;
