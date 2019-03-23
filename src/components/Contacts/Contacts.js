78ab87a8d658e61504cfcef8aa6372b64fae61f0
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-23 14:59:39 +0200

    Updated Contacts.js: - Added RadialMenu; - Added PropTypes; - Added styles;

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import RadialMenu from 'react-radial-menu';
 
-const Contacts = () => (
-    <div>CONTACTS</div>
-);
+const Contacts = props => {
+    const {contacts} = props;
+    return (
+        <div className="Contacts">
+            <RadialMenu
+                center={contacts.radialMenu.center}
+                items={contacts.radialMenu.items}
+                duration={contacts.radialMenu.duration}
+                stagger={contacts.radialMenu.stagger}
+                itemsSize={contacts.radialMenu.itemsSize}
+                distance={contacts.radialMenu.distance}
+            />
+        </div>
+    );
+};
+
+Contacts.propTypes = {
+    contacts: PropTypes.object.isRequired
+};
 
 export default Contacts;
