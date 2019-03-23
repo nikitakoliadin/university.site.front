8ac3e1aee1410435e6f017bec342018da42dc97f
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-23 14:55:23 +0200

    Updated reducers.js: - Added contactsReducer;

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
 import menuReducer from './menuReducer';
+import contactsReducer from './contactsReducer';
 
 const reducers = combineReducers({
     routing: routerReducer,
-    menu: menuReducer
+    menu: menuReducer,
+    contacts: contactsReducer
 });
 
 export default reducers;
