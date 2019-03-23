3a6087f398f3918f25c0fa19e92972ad64845a86
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-23 18:13:49 +0200

    Updates

---
+++ precache-manifest.5ee4dc24979500bc7660f1634821a3aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cd245cd1779c1f08969",
+    "url": "/university.site.front/static/js/runtime~main.9b1be3ca.js"
+  },
+  {
+    "revision": "77b6566cff36f60d96c7",
+    "url": "/university.site.front/static/js/main.0e2b5282.chunk.js"
+  },
+  {
+    "revision": "3699606bcc5ba265997f",
+    "url": "/university.site.front/static/js/2.d9577743.chunk.js"
+  },
+  {
+    "revision": "3699606bcc5ba265997f",
+    "url": "/university.site.front/static/css/2.0b3244c7.chunk.css"
+  },
+  {
+    "revision": "c1d8d74e0443b635ae6a67f1cecb3234",
+    "url": "/university.site.front/index.html"
+  }
+];
