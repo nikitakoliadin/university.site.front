d16774d59082e7ae35445dee6192d00434918c01
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-23 15:46:20 +0200

    Updated Contacts.js: - Added changeSize action; - Changed to class component; - Added resize event listeners;

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import RadialMenu from 'react-radial-menu';
 
-const Contacts = props => {
-    const {contacts} = props;
-    return (
-        <div className="Contacts">
-            <RadialMenu
-                center={contacts.radialMenu.center}
-                items={contacts.radialMenu.items}
-                duration={contacts.radialMenu.duration}
-                stagger={contacts.radialMenu.stagger}
-                itemsSize={contacts.radialMenu.itemsSize}
-                distance={contacts.radialMenu.distance}
-            />
-        </div>
-    );
-};
+class Contacts extends Component {
+
+    componentDidMount() {
+        const {changeSize} = this.props;
+        window.addEventListener('resize', () => changeSize());
+    }
+
+    componentWillUnmount() {
+        const {changeSize} = this.props;
+        window.removeEventListener('resize', () => changeSize());
+    }
+
+    render() {
+        const {contacts} = this.props;
+        return (
+            <div className="Contacts">
+                <RadialMenu
+                    center={contacts.radialMenu.center}
+                    items={contacts.radialMenu.items}
+                    duration={contacts.radialMenu.duration}
+                    stagger={contacts.radialMenu.stagger}
+                    itemsSize={contacts.radialMenu.itemsSize}
+                    distance={contacts.radialMenu.distance}
+                />
+            </div>
+        );
+    }
+}
 
 Contacts.propTypes = {
-    contacts: PropTypes.object.isRequired
+    contacts: PropTypes.object.isRequired,
+    changeSize: PropTypes.func.isRequired
 };
 
 export default Contacts;
