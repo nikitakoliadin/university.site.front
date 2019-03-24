e352bc7ae3c0241b9ae258c6036f7c35eb86abd9
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-24 18:23:07 +0200

    Updated reducers.js: - Added technologiesReducer;

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
 import menuReducer from './menuReducer';
+import technologiesReducer from './technologiesReducer';
 import contactsReducer from './contactsReducer';
 
 const reducers = combineReducers({
     routing: routerReducer,
     menu: menuReducer,
+    technologies: technologiesReducer,
     contacts: contactsReducer
 });
 
