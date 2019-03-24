0d8b86e9a4b3e3c51d40c106005d03c3af77f9e6
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-24 18:35:55 +0200

    Updated routes.js: - Updated AboutContainer Link;

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@ import {Route, IndexRedirect} from 'react-router';
 
 import AppContainer from './components/App/AppContainer';
 import Main from './components/Main/Main';
-import About from './components/About/About';
+import AboutContainer from './components/About/AboutContainer';
 import ContactsContainer from './components/Contacts/ContactsContainer';
 
 export default (
     <Route path="/" component={AppContainer}>
         <IndexRedirect to="main"/>
         <Route path="/main" component={Main}/>
-        <Route path="/about" component={About}/>
+        <Route path="/about" component={AboutContainer}/>
         <Route path="/contacts" component={ContactsContainer}/>
         <Route path="*" component={Main}/>
     </Route>
