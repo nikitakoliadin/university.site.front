ae23b2505f92ca490eaf966f513851b8dd40adcf
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-27 19:04:42 +0200

    Updated About.js: - Implements component;

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import CloseIcon from '@material-ui/icons/Add';
 
-const About = () => (
-    <span>ABOUT</span>
-);
+class About extends Component {
+
+    showListOfTechnologies() {
+        const {technologies, selectTechnology, openTechnology} = this.props;
+        return technologies.map((technology) => {
+            return (
+                <Button key={technology.id}
+                        variant="contained"
+                        className="Button"
+                        onClick={() => {
+                            selectTechnology(technology);
+                            openTechnology();
+                        }}>
+                    {technology.name}
+                </Button>
+            );
+        });
+    }
+
+    showDescription() {
+        const {activeTechnology, closeTechnology} = this.props;
+        return (
+            <div className="Description">
+                <hr/>
+                <div className="Content">
+                    <div className="Text">
+                        <div>{activeTechnology.technology.description}</div>
+                    </div>
+                    <div className="CloseButton">
+                        <Fab aria-label="Add"
+                             onClick={() => closeTechnology()}>
+                            <CloseIcon className="CloseLabel"/>
+                        </Fab>
+                    </div>
+                </div>
+                <hr/>
+                <br/>
+                <img src={activeTechnology.technology.imageSrc} className="Image" alt="logo"/>
+                <br/>
+                <br/>
+            </div>
+        );
+    }
+
+    render() {
+        const {activeTechnology} = this.props;
+        return (
+            <div className="About">
+                <div className="HeadText">
+                    This application is based on React.js and Redux.js. See description below.
+                </div>
+                <div className="SelectButtons">
+                    {this.showListOfTechnologies()}
+                </div>
+                {activeTechnology.showTechnology && this.showDescription()}
+            </div>
+        );
+    }
+}
+
+About.propTypes = {
+    technologies: PropTypes.array.isRequired,
+    activeTechnology: PropTypes.object.isRequired,
+    selectTechnology: PropTypes.func.isRequired,
+    openTechnology: PropTypes.func.isRequired,
+    closeTechnology: PropTypes.func.isRequired
+};
 
 export default About;
