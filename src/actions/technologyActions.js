ae638a3b4fd425be925752cd4ea136ac2bd62c08
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-28 17:51:23 +0200

    Updated technologyActions.js: - Added resetTechnology action;

--- src/actions/technologyActions.js
+++ src/actions/technologyActions.js
@@ -7,6 +7,13 @@ export const selectTechnology = (technology) => {
     };
 };
 
+export const resetTechnology = () => {
+    return {
+        type: types.RESET_TECHNOLOGY_TYPE,
+        payload: {}
+    };
+};
+
 export const openTechnology = () => {
     return {
         type: types.OPEN_TECHNOLOGY_INFO_TYPE,
