17873eb27fa4c5c090a20182250be5b6b3dee55a
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-30 15:18:44 +0200

    Updated reducers.js: - Added stylesReducer;

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -5,13 +5,15 @@ import menuReducer from './menuReducer';
 import technologiesReducer from './technologiesReducer';
 import activeTechnologyReducer from './activeTechnologyReducer.js';
 import contactsReducer from './contactsReducer';
+import stylesReducer from './stylesReducer'
 
 const reducers = combineReducers({
     routing: routerReducer,
     menu: menuReducer,
     technologies: technologiesReducer,
     activeTechnology: activeTechnologyReducer,
-    contacts: contactsReducer
+    contacts: contactsReducer,
+    styles: stylesReducer
 });
 
 export default reducers;
