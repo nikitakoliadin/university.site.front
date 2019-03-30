4b9a91c94f37ac81e07d396300f158e8b231add0
Nikita Koliadin <qThegamEp@gmail.com>
2019-03-30 16:10:00 +0200

    Updated Main.js: - Added styles; - Updated text;

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Main = () => (
-    <div>MAIN</div>
+    <div className="Main">
+        <span className="Text">
+            Never be afraid of losing people just
+            because you are afraid to be alone. You
+            are always alone.
+        </span>
+    </div>
 );
 
 export default Main;
