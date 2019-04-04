320b5b86484ef5377e8c60945c1ef6bcc827e8fe
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-04 19:24:03 +0300

    Updated ContactsContainer.js: - Deleted changeSize action;

--- src/components/Contacts/ContactsContainer.js
+++ src/components/Contacts/ContactsContainer.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
-import {changeSize} from '../../actions/sizeActions';
 import Contacts from './Contacts';
 
 const ContactsContainer = props => {
     const {contacts} = props;
-    const {changeSize} = props;
     return (
-        <Contacts contacts={contacts}
-                  changeSize={changeSize}
-        />
+        <Contacts contacts={contacts}/>
     );
 };
 
@@ -21,10 +16,4 @@ function mapStateToProps(state) {
     };
 }
 
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-        changeSize: changeSize
-    }, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ContactsContainer);
+export default connect(mapStateToProps)(ContactsContainer);
