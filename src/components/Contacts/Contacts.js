d2221565ebbf44a22023601a2eb1e8dedf6e64d4
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-04 19:22:59 +0300

    Updated Contacts.js: - Deleted changeSize action; - Deleted resize handler; - Changed to arrow function component;

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,48 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import throttle from 'lodash.throttle';
 import RadialMenu from 'react-radial-menu';
 
-class Contacts extends Component {
-
-    resize = throttle(() => {
-        const {changeSize} = this.props;
-        changeSize();
-    }, 500);
-
-    componentDidMount() {
-        window.addEventListener('resize', this.resize);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.resize);
-    }
-
-    render() {
-        const {contacts} = this.props;
-        return (
-            <div className="Contacts">
+const Contacts = props => {
+    const {contacts} = props;
+    return (
+        <div className="Contacts">
                 <span className="Text">
                     Thank you for paying attention to my site. All my contacts are listed below in the radial menu.
                 </span>
-                <div className="RadialMenu">
-                    <RadialMenu
-                        center={contacts.radialMenu.center}
-                        items={contacts.radialMenu.items}
-                        duration={contacts.radialMenu.duration}
-                        stagger={contacts.radialMenu.stagger}
-                        itemsSize={contacts.radialMenu.itemsSize}
-                        distance={contacts.radialMenu.distance}
-                    />
-                </div>
+            <div className="RadialMenu">
+                <RadialMenu
+                    center={contacts.radialMenu.center}
+                    items={contacts.radialMenu.items}
+                    duration={contacts.radialMenu.duration}
+                    stagger={contacts.radialMenu.stagger}
+                    itemsSize={contacts.radialMenu.itemsSize}
+                    distance={contacts.radialMenu.distance}
+                />
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 Contacts.propTypes = {
     contacts: PropTypes.object.isRequired,
-    changeSize: PropTypes.func.isRequired
 };
 
 export default Contacts;
