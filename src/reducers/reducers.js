b285ab333cf935687242e80f59020d726a30c59b
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-05 14:35:29 +0300

    Updated reducers.js: - Renamed contactsReducer to contactsComponentReducer;

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -4,15 +4,15 @@ import {routerReducer} from 'react-router-redux';
 import menuReducer from './menuReducer';
 import technologiesReducer from './technologiesReducer';
 import activeTechnologyReducer from './activeTechnologyReducer.js';
-import contactsReducer from './contactsReducer';
-import stylesReducer from './stylesReducer'
+import contactsComponentReducer from './contactsComponentReducer';
+import stylesReducer from './stylesReducer';
 
 const reducers = combineReducers({
     routing: routerReducer,
     menu: menuReducer,
     technologies: technologiesReducer,
     activeTechnology: activeTechnologyReducer,
-    contacts: contactsReducer,
+    contactsComponent: contactsComponentReducer,
     styles: stylesReducer
 });
 
