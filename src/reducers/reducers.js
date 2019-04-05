fa377e934747345780b28c8361d40dad34570aaa
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-05 15:18:23 +0300

    Refactored reducers.js

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,21 +1,21 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
+import stylesReducer from './stylesReducer';
 import menuReducer from './menuReducer';
+import menuComponentReducer from './menuComponentReducer';
 import technologiesReducer from './technologiesReducer';
 import activeTechnologyReducer from './activeTechnologyReducer.js';
 import contactsComponentReducer from './contactsComponentReducer';
-import stylesReducer from './stylesReducer';
-import menuComponentReducer from './menuComponentReducer';
 
 const reducers = combineReducers({
     routing: routerReducer,
+    styles: stylesReducer,
     menu: menuReducer,
+    menuComponent: menuComponentReducer,
     technologies: technologiesReducer,
     activeTechnology: activeTechnologyReducer,
-    contactsComponent: contactsComponentReducer,
-    styles: stylesReducer,
-    menuComponent: menuComponentReducer
+    contactsComponent: contactsComponentReducer
 });
 
 export default reducers;
