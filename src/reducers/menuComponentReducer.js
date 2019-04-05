ba524a1b137083a19484eb05e4ebd9a63216ab30
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-05 14:39:51 +0300

    Created menuComponentReducer.js

---
+++ src/reducers/menuComponentReducer.js
@@ -0,0 +1,16 @@
+export default function () {
+    return {
+        hamburgerMenu: {
+            width: 32,
+            height: 24,
+            strokeWidth: 3,
+            rotate: 0,
+            color: "black",
+            borderRadius: 0,
+            animationDuration: 0.5
+        },
+        cheeseburgerMenu: {
+            backgroundColor: "#eaeaea"
+        }
+    };
+};
