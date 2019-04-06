08310f3e3b608054fe3f2f759bf38a0097840973
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-06 14:08:35 +0300

    Created localization.js

---
+++ src/localization/localization.js
@@ -0,0 +1,73 @@
+import LocalizedStrings from 'react-localization';
+
+let strings = new LocalizedStrings({
+    en: {
+        header1: "University App",
+        menu1: "Main",
+        menu2: "About",
+        menu3: "Contacts",
+        main1: "Never be afraid of losing people just because you are afraid to be alone. You are always alone.",
+        about1: "This application is based on React.js and Redux.js. See description below.",
+        about2: "React",
+        about3: "In computing, React (also known as React.js or ReactJS) is a JavaScript library for " +
+            "building user interfaces. It is maintained by Facebook and a community of individual developers " +
+            "and companies. React can be used as a base in the development of single-page or mobile " +
+            "applications. Complex React applications usually require the use of additional libraries for " +
+            "state management, routing, and interaction with an API.",
+        about4: "Redux",
+        about5: "Redux is an open-source JavaScript library for managing application state. " +
+            "It is most commonly used with libraries such as React or Angular for building user interfaces. " +
+            "Similar to (and inspired by) Facebook's Flux architecture, it was created by " +
+            "Dan Abramov and Andrew Clark.",
+        contacts1: "Thank you for paying attention to my site. All my contacts are listed below in the radial menu.",
+        footer1: "University App",
+        footer2: "© qThegamEp, 2019"
+    },
+    ru: {
+        header1: "University App",
+        menu1: "Главная",
+        menu2: "О приложении",
+        menu3: "Контакты",
+        main1: "Никогда не бойся потерять людей только потому, что ты боишься быть один. Ты всегда один.",
+        about1: "Это приложение основано на React.js и Redux.js. Смотрите описание ниже.",
+        about2: "React",
+        about3: "React (иногда React.js или ReactJS) — JavaScript-библиотека с открытым исходным кодом для " +
+            "разработки пользовательских интерфейсов. React разрабатывается и поддерживается Facebook, Instagram " +
+            "и сообществом отдельных разработчиков и корпораций. React может использоваться для разработки " +
+            "одностраничных и мобильных приложений. Его цель — предоставить высокую скорость, простоту " +
+            "и масштабируемость.",
+        about4: "Redux",
+        about5: "Redux — шаблон для JavaScript с открытым исходным кодом, предназначенный для управления " +
+            "состоянием приложения. Чаще всего используется в связке с React или Angular для разработки " +
+            "клиентской части. Содержит ряд инструментов, позволяющих значительно упростить передачу данных " +
+            "хранилища через контекст. Создатели: Дэн Абрамов и Эндрю Кларк. Redux был разработан под влиянием " +
+            "Flux, разработанного в Facebook.",
+        contacts1: "Спасибо, что уделили внимание моему сайту. Все мои контакты перечислены ниже в радиальном меню.",
+        footer1: "University App",
+        footer2: "© qThegamEp, 2019"
+    },
+    uk: {
+        header1: "University App",
+        menu1: "Головна",
+        menu2: "Про програму",
+        menu3: "Контакти",
+        main1: "Ніколи не бійся втратити людей тільки тому, що ти боїшся бути один. Ти завжди один.",
+        about1: "Ця програма базується на React.js і Redux.js. Дивіться опис нижче.",
+        about2: "React",
+        about3: "React (старі назви: React.js, ReactJS) — відкрита JavaScript бібліотека для створення " +
+            "інтерфейсів користувача, яка покликана вирішувати проблеми часткового оновлення вмісту веб-сторінки, " +
+            "з якими стикаються в розробці односторінкових застосунків. Розробляється Facebook, Instagram і " +
+            "спільнотою індивідуальних розробників. React дозволяє розробникам створювати великі веб-застосунки, " +
+            "які використовують дані, котрі змінюються з часом, без перезавантаження сторінки. Його мета полягає в " +
+            "тому, щоб бути швидким, простим, масштабованим.",
+        about4: "Redux",
+        about5: "Redux - відкрита JavaScript бібліотека призначена для управління станом програми. Найчастіше " +
+            "використовується разом з React або Angular для побудови інтерфейсів користувача. Redux був створений " +
+            "Деном Абрамовим і Ендрю Кларком. Redux був створений під впливом Flux, розробленого Facebook.",
+        contacts1: "Дякуємо, що звернули увагу на мій сайт. Усі мої контакти перелічені нижче в радіальному меню.",
+        footer1: "University App",
+        footer2: "© qThegamEp, 2019"
+    }
+});
+
+export default strings;
