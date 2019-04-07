761e12dbc232bec2938707000f46196f2001f748
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-07 13:27:55 +0300

    Updated languageActions.js: - Added selectDefaultLanguage action;

--- src/actions/languageActions.js
+++ src/actions/languageActions.js
@@ -1,6 +1,14 @@
 import * as types from '../constants/actionTypes';
 import localization from '../localization/localization';
 
+export const selectDefaultLanguage = (language) => {
+    localization.setLanguage(language.name);
+    return {
+        type: types.SELECT_DEFAULT_LANGUAGE_TYPE,
+        payload: language
+    };
+};
+
 export const selectLanguage = (language) => {
     localization.setLanguage(language.name);
     return {
