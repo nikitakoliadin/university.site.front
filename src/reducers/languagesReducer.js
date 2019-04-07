cf0baea8d9b7763737d569bc281d5cd205dd167a
Nikita Koliadin <qThegamEp@gmail.com>
2019-04-07 11:52:59 +0300

    Created languagesReducer.js

---
+++ src/reducers/languagesReducer.js
@@ -0,0 +1,16 @@
+export default function () {
+    return [
+        {
+            id: 1,
+            name: "en"
+        },
+        {
+            id: 2,
+            name: "ru"
+        },
+        {
+            id: 3,
+            name: "uk"
+        }
+    ];
+};
